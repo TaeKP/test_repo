90d7a4ecccb533a63d98298374899d50ba8f5f34
TaeKP <pskitipat@gmail.com>
2023-11-17 09:46:08 +0100

    edit script

--- test_script.R
+++ test_script.R
@@ -8,22 +8,13 @@ library(openCR)
 # 3 single session
 
 # 1st session; year 2019
-FC2019 <- read.capthist("caphist2019_L.csv", "trap2019_covs.csv", detector = "proximity", noccasions = 75, 
-                        covnames = "sex", trapcovnames = c("trap_night", "hunt_lev", "Aqu", "Agr", "Aba", 
-                                                           "Nat", "Unu", "Hum", "oneC5", "twoC5", "threeC5", "allc5", "Dist_road", "Dist_railway",
-                                                           "rodent", "aquB", "ter_aeB", "ter_arB", "total_B"))
+FC2019 <- read.capthist("caphist2019_L.csv", "trap2019_covs.csv", detector = "proximity", noccasions = 75, covnames = "sex", trapcovnames = "trap_night")
 
 # 2nd session; year 2021
-FC2021 <- read.capthist("caphist2021_L.csv", "trap2021_covs.csv", detector = "proximity", noccasions = 148, 
-                        covnames = "sex", trapcovnames = c("trap_night", "hunt_lev", "Aqu", "Agr", "Aba", 
-                                                           "Nat", "Unu", "Hum", "oneC5", "twoC5", "threeC5", "allc5", "Dist_road", "Dist_railway",
-                                                           "rodent", "aquB", "ter_aeB", "ter_arB", "total_B"))
+FC2021 <- read.capthist("caphist2021_L.csv", "trap2021_covs.csv", detector = "proximity", noccasions = 148, covnames = "sex", trapcovnames = "trap_night")
 
 # 3rd session; year 2023
-FC2023 <- read.capthist("caphist2023_L.csv", "trap2023_covs.csv", detector = "proximity", noccasions = 129, 
-                        covnames = "sex", trapcovnames = c("trap_night", "hunt_lev", "Aqu", "Agr", "Aba", 
-                                                           "Nat", "Unu", "Hum", "oneC5", "twoC5", "threeC5", "allc5", "Dist_road", "Dist_railway",
-                                                           "rodent", "aquB", "ter_aeB", "ter_arB", "total_B"))
+FC2023 <- read.capthist("caphist2023_L.csv", "trap2023_covs.csv", detector = "proximity", noccasions = 129, covnames = "sex", trapcovnames = "trap_night")
 
 par(mfrow=c(1,3))
 plot(FC2019, rad=50, track=TRUE)
